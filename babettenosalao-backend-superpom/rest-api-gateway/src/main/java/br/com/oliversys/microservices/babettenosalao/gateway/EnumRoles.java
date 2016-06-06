9ea098952ae7d595171583fe7bcde56e92def7ca
William Alves <alves.bill@gmail.com>
2016-06-06 10:07:22 -0300

    - add script linux "gerar-jks.sh"; - add script sql "schema.sql" de objetos de gravacao do token;

--- babettenosalao-backend-superpom/rest-api-gateway/src/main/java/br/com/oliversys/microservices/babettenosalao/gateway/EnumRoles.java
+++
@@ -1,5 +0,0 @@
-package br.com.oliversys.microservices.babettenosalao.gateway;
-
-public enum EnumRoles {
-	USUARIO,SUPER_USUARIO,ADMIN;
-}
--- babettenosalao-backend-superpom/rest-api-gateway/src/main/java/br/com/oliversys/microservices/babettenosalao/gateway/SecurityConfig.java
+++ babettenosalao-backend-superpom/rest-api-gateway/src/main/java/br/com/oliversys/microservices/babettenosalao/gateway/SecurityConfig.java
@@ -1,107 +1,46 @@
 package br.com.oliversys.microservices.babettenosalao.gateway;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 
-import javax.servlet.Filter;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.filter.CompositeFilter;
-
 
 @Configuration
-//@Order(6)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	OAuth2ClientContext oauth2ClientContext;
-	
-	@Autowired
-	public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")
-				.password("password").roles("USER", "ADMIN");
-	}
-
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers(HttpMethod.OPTIONS).permitAll()
-				.anyRequest().authenticated().and()
-				//.httpBasic()				
-				.csrf().disable()
-				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-	}
-	
-	@Configuration
-	@EnableResourceServer
-	protected static class ResourceServerConfiguration
-			extends ResourceServerConfigurerAdapter {
-		@Override
-		public void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-				.antMatcher("/user")
-				.authorizeRequests().anyRequest().authenticated();
-			// @formatter:on
-		}
-	}
-	
-	private Filter ssoFilter() {
-		CompositeFilter filter = new CompositeFilter();
-		List<Filter> filters = new ArrayList<>();
-		filters.add(ssoFilter(facebook(), "/login/facebook"));
-		//filters.add(ssoFilter(github(), "/login/github"));
-		filter.setFilters(filters);
-		return filter;
-	}
-	
-	private Filter ssoFilter(ClientResources client, String path) {
-		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(path);
-		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(client.getClient(),oauth2ClientContext);
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-		facebookFilter.setRestTemplate(facebookTemplate);
-		facebookFilter.setTokenServices(new UserInfoTokenServices(
-				client.getResource().getUserInfoUri(), client.getClient().getClientId()));
-		
-		return facebookFilter;
-	}
-	
-	@Bean
-	@ConfigurationProperties("facebook")
-	ClientResources facebook() {
-		return new ClientResources();
-	}
-	
-	class ClientResources {
-		private OAuth2ProtectedResourceDetails client = new AuthorizationCodeResourceDetails();
-		private ResourceServerProperties resource = new ResourceServerProperties();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                		(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+            .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+            .and()
+                .httpBasic();
+    }
 
-		public OAuth2ProtectedResourceDetails getClient() {
-			return client;
-		}
-
-		public ResourceServerProperties getResource() {
-			return resource;
-		}
-	}
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("reader")
+                .password("reader")
+                .authorities("FOO_READ")
+                .and()
+                .withUser("writer")
+                .password("writer")
+                .authorities("FOO_READ", "FOO_WRITE");
+    }
 }
